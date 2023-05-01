d0c205f78c345a213a5cc2ec88ffb39c1596cdf6
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-05-01 21:02:28 +0530

    final

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors());
  const PORT= 5500;
 
 app.use(express.json());
-app.use("/",()=>console.log("BookMyShow-App"))
 app.use("/movies", moviesRouter);
 app.use("/users", userRouter);
 
